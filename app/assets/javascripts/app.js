7c8c9f14cbf0785446826aa78682bab21e595376
Tsutsumi,Yosuke | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2017-05-19 14:04:29 +0900

    Finish : Integrating Flux into React Components (ES5 Architecture)

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -29,14 +29,27 @@ var Store = new _.extend({}, EventEmitter.prototype, {
   }
 });
 
-var AppDispatcher = new FluxDispatcher();
+var AppDispatcher = new Flux.Dispatcher();
+
 AppDispatcher.register(function(payload) {
   var action = payload.actionType;
+  console.log(payload)
   switch(action) {
     case Constants.ADD_COMMENT:
     Store.addComment(payload.comment);
+    Store.emitChange();
     break;
   default:
+    // NO-OP
+  }
+});
 
+// Actions
+var Actions = new _.extend({}, {
+  addComment: function(params) {
+    AppDispatcher.dispatch({
+      actionType: Constants.ADD_COMMENT,
+      comment: params
+    })
   }
 })
--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -1,11 +1,26 @@
 var CommentList = React.createClass({
+
+  componentDidMount: function() {
+    Store.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function() {
+    Store.removeChangeListener(this._onChange);
+  },
+
   render: function() {
+    console.log('rendering');
     return (
       <div>
-        {[].map(function(comment){
+        {Store.comments().map(function(comment){
           return <Comment key={comment.id} {... comment} />;
         })}
       </div>
     )
+  },
+
+  _onChange: function() {
+    this.forceUpdate();
   }
+
 })
